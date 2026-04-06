83ec4c279a479e844db34634a226011b66ab13d3
Dylan Walker <127539444+Dylidan@users.noreply.github.com>
2026-04-06 14:55:29 -0400

    aprilsixth

---
+++ README.md
@@ -0,0 +1,9 @@
+# talk_assist
+
+This is Flutter project.
+
+To run this project:
+
+```
+flutter run
+```
---
+++ android/app/src/main/kotlin/com/example/talk_assist/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.talk_assist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
